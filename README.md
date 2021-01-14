584c093dd95a4d0d321fe46f168965b68123724e
Maja Reissner <maja.reissner@sidn.nl>
2021-01-14 09:45:38 +0100

    Update day2 after receiving feedback.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://adventofcode.com/
+
+
+Run locally with:  
+go run github.com/synaptic-cleft/adventOfCode
--- day2/day2.go
+++ day2/day2.go
@@ -10,18 +10,15 @@ import (
 )
 
 
-var correctPasswordCount int = 0
-var correctPasswordCountSecondRuleSet int = 0
+var correctPasswordCount = 0
+var correctPasswordCountSecondRuleSet = 0
 
 func Solve() {
-	fmt.Println("hoi")
-
-	// read input file
-	// FIX: relative path
-	file, error := os.Open("/Users/maja/gitRepo/adventOfCode/day2/input.txt")
+	file, error := os.Open("./day2/input.txt")
 
 	if error != nil {
 		fmt.Println("Could not read file.")
+		os.Exit(1)
 	}
 
 	defer file.Close()
@@ -30,16 +27,21 @@ func Solve() {
 
 
 	for scanner.Scan() {
-		// lines = append(lines, scanner.Text())
 		line := strings.Fields(scanner.Text())
 		amount, letterField, password := line[0], line[1], line[2]
 		amounts := strings.Split(amount, "-")
-		firstNumber, _ := strconv.Atoi(amounts[0])
-		secondNumber, _ := strconv.Atoi(amounts[1])
+		firstNumber, e1 := strconv.Atoi(amounts[0])
+		secondNumber, e2 := strconv.Atoi(amounts[1])
+
+		if e1 != nil || e2 != nil {
+			fmt.Println("You promised the input would contain integers only. Why would you lie to me?")
+			os.Exit(1)
+		}
+
 		letter := strings.Split(letterField, ":")[0]
 
-		verifyIfMinAndMaxAmountOfConstraintIsMet(password, letter, firstNumber, secondNumber)
-		verifyExactlyOnceOccurence(password, []byte(letter)[0], firstNumber, secondNumber)
+		countCorrectPasswords(password, letter, firstNumber, secondNumber)
+		countExactlyOnceOccurrence(password, []byte(letter)[0], firstNumber, secondNumber)
 	}
 
 	fmt.Println("correctPasswordCount", correctPasswordCount)
@@ -47,7 +49,7 @@ func Solve() {
 
 }
 
-func verifyIfMinAndMaxAmountOfConstraintIsMet(pw string, letter string, min int, max int) {
+func countCorrectPasswords(pw string, letter string, min int, max int) {
 	regex := regexp.MustCompile(letter)
 
 	occurenceOfNumberInPassword := len(regex.FindAllStringIndex(pw, -1))
@@ -57,7 +59,7 @@ func verifyIfMinAndMaxAmountOfConstraintIsMet(pw string, letter string, min int,
 	}
 }
 
-func verifyExactlyOnceOccurence(pw string, letter byte, pos1 int, pos2 int) {
+func countExactlyOnceOccurrence(pw string, letter byte, pos1 int, pos2 int) {
 	if (pw[pos1-1] == letter && pw[pos2-1] != letter) || (pw[pos1-1] != letter && pw[pos2-1] == letter) {
 		correctPasswordCountSecondRuleSet++
 	}
